caef24818f6e1cedac3d7ab1bf7cf70d816a68b0
BugHunter-X <askzenai@gmail.com>
2025-02-19 07:16:05 +0100

    Add UncommonBugSolution.java file

---
+++ UncommonBugSolution.java
@@ -0,0 +1,14 @@
+public class UncommonBugSolution {
+
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 0;
+        int z = 0; 
+        if (y != 0) {
+            z = x / y; 
+        } else {
+            System.out.println("Cannot divide by zero.");
+        }
+        System.out.println(z);
+    }
+} 
